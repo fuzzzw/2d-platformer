fdd4051ac359ee9a8782d91e41f3dc6d1a08ecb8
David Christensen <fuzzz.org@gmail.com>
2017-08-25 00:30:56 +0200

    Refactor player:update()  - removed one for-loop

--- src/main.lua
+++ src/main.lua
@@ -38,7 +38,7 @@ function love.draw()
 
   --debug
   draw.debug("x:"..math.floor(player.collision.x)..", y:"..math.floor(player.collision.y),0)
-  draw.debug("player.y_velocity: "..math.floor(player.collision.y_velocity),1)
-  draw.debug("screen width: "..love.graphics.getWidth()..
-             ", screen height: "..love.graphics.getHeight(),2)
+  draw.debug("y_velocity: "..math.floor(player.collision.y_velocity),1)
+  --draw.debug("screen width: "..love.graphics.getWidth()..
+  --           ", screen height: "..love.graphics.getHeight(),2)
 end
--- src/player.lua
+++ src/player.lua
@@ -9,8 +9,8 @@ function Player:new(obj)  -- The constructor
   Player.super.new(self,{
     x = obj.x or 10,
     y = obj.y or 300,
-    w = obj.w or 32,
-    h = obj.h or 32
+    w = obj.w or 30,
+    h = obj.h or 30
   })
 
   self.speed       = obj.speed or 200
@@ -19,14 +19,12 @@ function Player:new(obj)  -- The constructor
   self.gravity     = obj.gravity or -1000
 end
 
-local function fallingUpdate(player,entities,dt)
+local function y_axis_update(player,entities,dt)
   if player.y_velocity == 0 then
     if love.keyboard.isDown('space') then
       player.y_velocity = player.jump_height
     else
-      if not player:check_entities(entities) then
-        player.y_velocity = 1
-      end
+      player.y_velocity = 1
     end
   end
 
@@ -34,9 +32,7 @@ local function fallingUpdate(player,entities,dt)
     player.y = player.y + player.y_velocity * dt
     player.y_velocity = player.y_velocity - player.gravity * dt
   end
-end
 
-local function groundOrCeilingBlock(player,entities,dt)
   if player.y_velocity > 0 then
     local entity = player:check_entities(entities)
     if entity then
@@ -52,7 +48,7 @@ local function groundOrCeilingBlock(player,entities,dt)
   end
 end
 
-local function leftOrRightBlock(player,entities,dt)
+local function x_axis_update(player,entities,dt)
   if love.keyboard.isDown('right') then
     player.x = player.x + (player.speed * dt)
     local entity = player:check_entities(entities)
@@ -69,9 +65,8 @@ local function leftOrRightBlock(player,entities,dt)
 end
 
 function Player:update(map,dt)
-  fallingUpdate(self,map,dt)
-  groundOrCeilingBlock(self,map,dt)
-  leftOrRightBlock(self,map,dt)
+  y_axis_update(self,map,dt)
+  x_axis_update(self,map,dt)
 end
 
 return Player
